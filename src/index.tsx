7bef0c7cd379ee9ba7e98b7d8e5a0c1fe158c227
Markus J. Ankenbrand <iimog@iimog.org>
2019-09-07 20:27:14 +0200

    Create board

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,29 @@ import * as React from "react";
 import { render } from "react-dom";
 
 import "./styles.css";
+import "./logic/game";
+
+function Square() {
+  return <div className="Square" />;
+}
+
+function Board() {
+  let squares = Array(36);
+  for (let i = 0; i < 36; i++) {
+    squares[i] = <Square key={i} />;
+  }
+  return <div className="Board">{squares}</div>;
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <Board />
     </div>
   );
 }
 
 const rootElement = document.getElementById("root");
 render(<App />, rootElement);
-
-import "./logic/game";
